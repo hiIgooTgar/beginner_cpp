459a32e78efb43b53782183e9727efa69025571c
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-09-30 03:59:50 +0700

    Variabel dan Type data Update

--- latihan_1/variabel_typeData.cpp
+++ latihan_1/variabel_typeData.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() {
 	
-	string name; 
+	string name;
+	string studying;
+	string faculty; 
 	int age;
 	char gender;
 	
@@ -13,6 +15,14 @@ int main() {
 	cout<<"Answer : ";
 	getline(cin, name);
 	
+	cout<<"From which program study :  "<<endl;
+	cout<<"Answer : ";
+	cin >> studying;
+	
+	cout<<"From which faculty : "<<endl;
+	cout<<"Answer : ";
+	cin >> faculty;
+	
 	cout<<"How old are you : "<<endl;
 	cout<<"Answer : ";
 	cin >> age;
@@ -21,7 +31,9 @@ int main() {
 	cout<<"Answer : ";
 	cin >> gender;
 	
-	cout<<"Hello Cpp , "<<"My name is "<<name<<", Now I'am "<<age<< "Years old ";
+	
+	cout<<"Hello Cpp , "<<"My name is "<<name<<". From which program study "<<studying<<" Faculty "<<faculty
+	<<" , Now I'am "<<age<< " Years old ";
 	cout<<" and my gender "<<gender;
 
 	return 0;
