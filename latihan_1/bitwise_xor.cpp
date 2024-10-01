960ebfd5b2aaa6a4b44c855a65bdb935bbae68cb
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-01 17:15:47 +0700

    Bitwise XOR

---
+++ latihan_1/bitwise_xor.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a = 6;
+	int b = 3;
+	int hasil;
+	
+	hasil = a ^ b;
+	cout<<"Hasil a ^ b = "<<hasil<<endl;
+	return 0;
+}
+
+/* 
+
+Nama Operator	Simbol di Java
+AND				&
+OR				|
+XOR				^
+NOT/komplemen	~
+Left Shift		<<
+Right Shift		>>
+
+*/
