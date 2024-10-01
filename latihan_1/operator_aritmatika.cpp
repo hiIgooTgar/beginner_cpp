00286c2b48c415f99cfeae2c865603953526ddc7
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-01 16:34:23 +0700

    Operator Aritmatika dan Operator Penugasan / Assignment

---
+++ latihan_1/operator_aritmatika.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b;
+	// float a, b;
+	
+	cout<<"Value Pertama : ";
+	cin >> a;
+	
+	cout<<"Value Kedua : ";
+	cin >> b;
+	
+	
+	cout<<"Penjumlahan dari hasil value pertama + value kedua adalah : " << a + b <<endl; 
+	cout<<"Pengurangan dari hasil value pertama - value kedua adalah : " << a - b << endl;
+	cout<<"Perkalian dari hasil value pertama - value kedua adalah : " << a * b << endl;
+	cout<<"Pembagian dari hasil value pertama - value kedua adalah : " << a / b << endl;
+	cout<<"Modulus dari hasil value pertama - value kedua adalah : " << a % b << endl;
+	
+	return 0;
+}
+/*
+
+da enam jenis kelompok operator dalam pemrograman C++ yang harus kamu ketahui:
+
+Operator Aritmatika;
+Operator Penugasan;
+Operator Pembanding;
+Operator Logika;
+Operator Bitwise;
+dan Operator Lain-lain.
+ 
+*/
---
+++ latihan_1/operator_penugasan.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a, b;
+	a = 5;
+	b = 10;
+	
+	// b += a atau  b = b + a
+	b += a;
+	cout<<"Hasil dari b += a "<< b <<endl; 
+	
+	// b = a atau  b = b - a
+	b -= a;
+	cout<<"Hasil dari b -= a "<< b <<endl;
+	
+	// b *= a atau  b = b * a
+	b *= a;
+	cout<<"Hasil dari b *= a "<< b <<endl;
+	
+	// b /= a atau  b = b / a
+	b /= a;
+	cout<<"Hasil dari b /= a "<< b <<endl;
+	
+	// b %= a atau  b = b % a
+	b %= a;
+	cout<<"Hasil dari b %= a "<< b <<endl;
+	
+	return 0;
+}
+
+
