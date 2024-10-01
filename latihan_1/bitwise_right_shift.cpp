824b567592bad53ead868ebd2a1ab1aa42d0ab93
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-01 17:27:10 +0700

    Bitwise Right Shift

---
+++ latihan_1/bitwise_right_shift.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a = 6;
+	int hasil;
+	
+	hasil = a >> 1;
+	cout<<"Hasil a >> 1 = "<<hasil<<endl;
+	return 0;
+}
+
+
+/* 
+
+Nama Operator	Simbol di Java
+AND				&
+OR				|
+XOR				^
+NOT/komplemen	~
+Left Shift		<<
+Right Shift		>>
+
+*/
