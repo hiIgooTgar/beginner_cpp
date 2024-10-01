4b10e61a8f1eeab246551388f9c994fb74539da7
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-01 17:20:36 +0700

    Bitwise NOT

---
+++ latihan_1/bitwise_not.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int a = 6;
+	int hasil;
+	
+	hasil = ~a;
+	cout<<"Hasil ~a = "<<hasil<<endl;
+	return 0;
+}
+
+
+/* 
+
+Bitwise NOT dikenal juga dengan komplemen.
+
+Nama Operator	Simbol di Java
+AND				&
+OR				|
+XOR				^
+NOT/komplemen	~
+Left Shift		<<
+Right Shift		>>
+
+*/
