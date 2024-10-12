712feba63fa0e295502d42a1a2c5c9cfa2829394
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-12 20:24:55 +0700

    learn Cpp | Beginner - Operator Aritmatika

---
+++ learning_practice_2/operator_aritmatika.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	
+	int a;
+	int b;
+	
+	printf("===== Operator Aritmatika =====\n\n");
+	printf("Nilai Pertama : ");
+	scanf("%i", &a);
+	printf("Nilai Kedua : ");
+	scanf("%i", &b);
+	
+	cout<<endl;
+	
+	if(a % 2 == 0) {
+		cout<<a << " adalah bilangan genap"<<endl;
+	} else {
+		cout<<a << " adalah bilangan ganjil"<<endl;
+	}
+		
+	if(b % 2 == 0) {
+		cout<<b << " adalah bilangan genap"<<endl;
+	} else {
+		cout<<b << " adalah bilangan ganjil"<<endl;
+	}
+	cout<<endl;
+	
+	cout<<"Penjumlahan dari "<<a<<" + " << b << " Maka hasilnya " << a + b<<endl;
+	printf("Pengurangan dari %i - %d Maka hasilnya %i \n", a , b, a - b);
+	cout<<"Perkalian dari "<<a<<" * "<<b<<" Maka hasilnya "<<a * b<<endl;
+	printf("Pembagian dari %i / %i Maka hasiknya %d \n",a ,b , a / b);
+	cout<<"Modulus Sisa bagi dari "<<a<<" % "<<b<<" Maka hasilnya "<<a % b<<endl;
+	
+	cout<<endl;
+	printf("========= End ========== \n");
+
+	
+	return 0;
+}
