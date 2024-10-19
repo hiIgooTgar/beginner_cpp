448ebc4aa8cb20d2b6af3310097bfdf74bd86c82
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-19 23:42:39 +0700

    learn Cpp | Beginner - Array

---
+++ learning_practice_2/array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	string nama[] = {"C++", "C", "C#", "Java", "PHP", "Javascript"};
+	cout<<"Array [0] : "<<nama[0]<<endl;
+	printf("Array [1] : %s \n", nama[1].c_str());
+	cout<<"Array [2] : "<<nama[2]<<endl;
+	printf("Array [3] : %s \n", nama[3].c_str());
+	cout<<"Array [4] : "<<nama[4]<<endl;
+	printf("Array [5] : %s \n", nama[5].c_str());	
+	
+	return 0;
+}
