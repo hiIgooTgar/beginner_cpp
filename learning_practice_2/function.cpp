5bd60bebf00f645a601f4bea73e96274210f8cbf
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-10-21 07:49:35 +0700

    learn Cpp | Beginner - Function Rekursif

--- learning_practice_2/function.cpp
+++ learning_practice_2/function.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-string introduction(string nama, string nim, string prodi, string fakultas, string alamat) {
+void introduction(string nama, string nim, string prodi, string fakultas, string alamat) {
+	cout<<"== Data Student =="<<endl;
 	cout<<"Name : "<<nama<<endl;
 	cout<<"NIM : "<<nim<<endl;
 	printf("Program Study : %s \n", prodi.c_str());
@@ -9,9 +10,25 @@ string introduction(string nama, string nim, string prodi, string fakultas, stri
 	printf("Address:  %s \n", alamat.c_str());
 }
 
+void learning(string lesson);
+void programming(string language);
+
 int main() {
-	cout<<"== Data Student =="<<endl
 	introduction("Igo Tegar Prambudhy", "24SA11A159", "Informatics", "Computer Science", "Indonesian");
+	cout<<endl<<endl;
+	programming("C++, C, C#, Java, Python, Javascript, PHP");
+	cout<<endl<<endl;
+	learning("Software Engineering, Systrem Basis Data, Algorithm and Structure Data, Artificial Intelligence");
 	
 	return 0;
+}
+
+void programming(string language) {
+	printf("== Programming ==\n");
+	cout<<"Language : "<<language<<endl;
+}
+
+void learning(string lesson) {
+	printf("== Learning Informatic ==\n");
+	printf("Lesson : %s \n", lesson.c_str());
 }
