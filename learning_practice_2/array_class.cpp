e402950c665b4c0e4fcf2780b2d47695fb258067
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-04 21:33:28 +0700

    learn CPP | Beginner - Array Class

---
+++ learning_practice_2/array_class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+int main() {
+	array<string, 5> name = {"C++", "C", "Javascript", "Python", "PHP"};
+	
+	printf("As many array id %i : \n", name.size());
+	
+	for(int a = 0; a < name.size(); a++) {
+		cout<<". "<< name[a]<<endl
+	}
+	
+	return 0;
+}
