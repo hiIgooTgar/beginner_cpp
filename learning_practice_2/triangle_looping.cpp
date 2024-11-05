3464e33b76eb0d4f9c252616a15821b480739827
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-05 21:12:55 +0700

    learn CPP | Beginner - * Looping

--- learning_practice_2/triangle_looping.cpp
+++ learning_practice_2/triangle_looping.cpp
@@ -11,14 +11,37 @@ int main() {
 	
 	for(int a = 0; a <= triangle; a++) {
 		for(int b = 0; b <= a; b++) {
-			cout<<"* ";
+			cout<<"*";
 		}
 		cout<<endl;
 	}
 	
 	for(int a = triangle; a >= 0; a--) {
 		for(int b = 0; b <= a; b++) {
-			cout<<"* ";
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+	
+	for(int a = triangle; a >= 0; a--) {
+		for(int b = 0; b <= triangle; b++) {
+			if(b >= a) {
+				cout<<"*";
+			} else {
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}  
+	
+	for(int a = triangle; a >= 0 ; a--) {
+		for(int b = 0; b <= triangle - a; b++) {
+			cout<<"";
+			int z = 2 * triangle - 2;
+			z = z - 2;
+		}	
+		for(int b = 0; b <= a; b++) {
+			cout<<"*";
 		}
 		cout<<endl;
 	}
