a59b1c9728dfb0a53be85dba776abcdd63501615
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-13 04:59:36 +0700

    LKS Kabupaten Purbalingga 2024 | Web Technologies

--- learning_practice_2/practice_1_cin.cpp
+++ learning_practice_2/practice_1_cin.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void inputCin(string input) {
-	cout<<"Masukkan NIM : "; 
-	cin >> input;
-}
 
 int main() {
+	string nim;
 	string nama;
 	string jurusan;
+	string univ;
+	char jk;
+		
+	cout<<"Masukan NIM : ";
+	cin >> nim;
 	
 	cout<<"Masukan Nama : ";
 	cin >> nama;
 	
-	inputCin("24SA11A");
-	
 	cout<<"Masukan Jurusan : ";
 	cin >> jurusan;
+		
+	cout<<"Masukan Jenis Kelamin | Pilih (L/P) : ";
+	cin >> jk;
+	
+	cout<<"Masukan Nama Universitas : ";
+	cin >> univ;
 	
 	cout<<endl<<endl;
+
 	
 	cout<<"== Biodata =="<<endl;
 	cout<<"Nama : "<<nama<<endl;
-	cout<<"NIM : "<<inputCin<<endl;
+	cout<<"NIM : "<<nim<<endl;
 	cout<<"Jurusan : "<<jurusan<<endl;
-	
+	if(jk == 'L' || jk == 'l') {
+		cout<<"Jenis Kelamin : "<<"Laki-laki"<<endl;
+	} else if(jk == 'P' || jk == 'p') {
+		cout<<"Jenis Kelamin : "<<"Perempuan"<<endl;
+	}
+	cout<<"Universitas : "<<univ<<endl;
 }
